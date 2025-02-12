3450d21760002130dd183d4872b009a72d9e48ed
Moody Liu <mooodyhunter@outlook.com>
2025-02-12 20:20:44 +0800

    fix
    format code

--- sysdeps/mos/generic/entry.cpp
+++ sysdeps/mos/generic/entry.cpp
@@ -4,19 +4,14 @@
 #include <stdlib.h>
 #include <sys/auxv.h>
 
-// defined by the POSIX library
-void __mlibc_initLocale();
-
-extern "C" uintptr_t *__dlapi_entrystack();
 extern "C" void __dlapi_enter(uintptr_t *entry_stack);
 
 extern char **environ;
-static mlibc::exec_stack_data __mlibc_stack_data;
 
 typedef int (*main_fn_type)(int argc, char *argv[], char *env[]);
 
 extern "C" void __mlibc_entry(uintptr_t *entry_stack, main_fn_type main_fn) {
 	__dlapi_enter(entry_stack);
-	auto result = main_fn(__mlibc_stack_data.argc, __mlibc_stack_data.argv, environ);
+	auto result = main_fn(mlibc::entry_stack.argc, mlibc::entry_stack.argv, environ);
 	exit(result);
 }
